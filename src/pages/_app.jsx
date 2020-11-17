ac68d588a2809b4a47b400678f416a0e1898e79a
damishra <dxm2269@rit.edu>
2020-11-17 05:04:36 -0500

    init: initialize repository with project structure, dependency management, and licensing

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -10,6 +10,11 @@ const Root = ({ Component, ...pageProps }) => {
         <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
         <link rel='icon' href='/favicon.ico' type='image/x-icon' />
         <meta name='description' content="Dishant Mishra's portfolio" />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inconsolata:wght@400;600;800&family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap'
+          rel='stylesheet'
+        />
       </Head>
       <Component {...pageProps} />
     </>
